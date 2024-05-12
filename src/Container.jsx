6b3f85532a051d0b8213356f377715fa48b9ce5f
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-11 23:00:42 -0700

    stub website components

---
+++ src/Container.jsx
@@ -0,0 +1,11 @@
+import Stats from "./Stats";
+import TextArea from "./TextArea";
+
+export default function Container() {
+  return (
+    <main>
+      <TextArea />
+      <Stats />
+    </main>
+  );
+}
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; Copyright by VM. All rights reserved.</small>
+      <small>Last checked limits: 17 days ago</small>
+    </footer>
+  );
+}
