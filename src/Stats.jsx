7c3a1c5b900dcc5cda31ec37256bffb089169c7d
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-11 23:56:36 -0700

    stub stats component

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,19 @@
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat number={0} label="words" />
+      <Stat number={0} label="characters" />
+      <Stat number={280} label="instagram" />
+      <Stat number={2200} label="facebook" />
+    </section>
+  );
+}
+
+function Stat({ number, label }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
 }
